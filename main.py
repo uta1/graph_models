dabea54f8d88509257809a1fbc2024c56d248311
uta123 <uta123@yandex-team.ru>
2021-02-28 13:52:38 +0300

    resize ref

--- main.py
+++ main.py
@@ -124,7 +124,13 @@ def get_rects_by_contours(contours):
     return [rect for rect in rects if rect[-2] >= MIN_OBJECT_WIDTH and rect[-1] >= MIN_OBJECT_HEIGHT]
 
 
-def create_trg_image(image_name, target_size=(512, 512), print_bboxes=False):
+def resize(orig, target_size):
+    if target_size:
+        return cv2.resize(orig, target_size, interpolation = cv2.INTER_NEAREST)
+    return orig
+
+
+def create_trg_image(image_name, target_size=(512, 512), print_bboxes=True):
     if not image_name.startswith('PMC'):
         return
 
@@ -134,12 +140,13 @@ def create_trg_image(image_name, target_size=(512, 512), print_bboxes=False):
 
     th, im_gray_th_otsu = cv2.threshold(im_gray, 0, 255, cv2.THRESH_OTSU)
 
-    bined_resized = cv2.resize(im_gray_th_otsu, target_size, interpolation = cv2.INTER_NEAREST)
+    bined_resized = resize(im_gray_th_otsu, target_size)
 
     if print_bboxes:
         contours, hier = cv2.findContours(bined_resized, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
 
-        res_image = cv2.resize(original, target_size, interpolation = cv2.INTER_NEAREST)
+        res_image = resize(original, target_size)
+
         rects = get_rects_by_contours(contours)
 
         for x, y, w, h in rects:
@@ -148,6 +155,7 @@ def create_trg_image(image_name, target_size=(512, 512), print_bboxes=False):
         res_image = bined_resized
 
     cv2.imwrite(BINFOLDER + 'trg_' + image_name + '.tiff', res_image)
+    return res_image
 
 
 if __name__ == '__main__':
@@ -158,7 +166,7 @@ if __name__ == '__main__':
         print('downloading finished')
     if 'create_trg_image' in MODULES:
         for image in get_file_names(FOLDER):
-            create_trg_image(image)
+            create_trg_image(image, target_size=None)
     if 'main' in MODULES:
         process()
 
