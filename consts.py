e9a7f7e3aa7781987b40630cadff9837dd4504a9
uta1 <geymer_98@mail.ru>
2021-04-12 13:37:49 +0300

    feat labels creating: json-labels for rects (#12)
    feat labels creating: json-labels for rects

--- consts.py
+++ consts.py
@@ -1,11 +1,12 @@
 MODULES = ['create_trg_image']
 MODE = 'samples'
 LABELS = MODE + '.json'
-ARCHIVENAME = 'examples.tar.gz'
-TARFOLDER = 'examples/'
-FOLDER = '../examples/'
-BINFOLDER = FOLDER[:-1] + 'bin/'
-URL = 'https://dax-cdn.cdn.appdomain.cloud/dax-publaynet/1.0.0/' + ARCHIVENAME
+
+DATASETFOLDER = '../trg/'
+FOLDER = DATASETFOLDER + 'examples/'
+BINFOLDER = DATASETFOLDER + 'examplesbin/'
+LABELSFOLDER = DATASETFOLDER + 'exampleslabels/'
+
 MIN_OBJECT_WIDTH = 4
 MIN_OBJECT_HEIGHT = 4
 RECTS_DILATION = (7, 6)
--- downloader.py
+++ downloader.py
@@ -1,18 +1,27 @@
 from lib_imports import *
 from consts import *
+from utils import *
+
+
+ARCHIVENAME = 'examples.tar.gz'
+URL = 'https://dax-cdn.cdn.appdomain.cloud/dax-publaynet/1.0.0/' + ARCHIVENAME
+TARFOLDER = 'examples/'
 
 
 def download():
     r = requests.get(URL)
     open(ARCHIVENAME , 'wb').write(r.content)
-    
+
+
 def extract():
+    create_path(FOLDER)
     tar = tarfile.open(ARCHIVENAME)
     tar.extractall()
     tar.close()
     os.system('rm ' + ARCHIVENAME)
-    os.system('mv ' + TARFOLDER + ' ' + FOLDER)
-    
+    os.system('mv ' + TARFOLDER + ' ' + DATASETFOLDER)
+
+
 def get_samples():
     with open(FOLDER + LABELS, 'r') as fp:
         samples = json.load(fp)
