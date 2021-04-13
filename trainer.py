a2801fc3ffd06f158abb3a5d2b8fcc24b85e86f5
uta1 <uta123@yandex-team.ru>
2021-04-13 17:35:18 +0300

    base

--- trainer.py
+++ trainer.py
@@ -6,14 +6,12 @@ from utils import *
 
 
 def train():
+    model = unet(input_size=(*TARGET_SIZE, 1))
+
     cached_labels, image_id_by_file_name = cache_and_get_indices()
     for _, image_id in image_id_by_file_name.items():
-        # TODO: organize training by image_id
-        pass
-
-    model = unet(input_size=(*TARGET_SIZE, 1))
-    for image in get_file_names(BINS_FOLDER):
-        img = cv2.imread(BINS_FOLDER + image)
-        img = np.expand_dims(img[:, :, 2:], axis=0)
-        print(img.shape)
-        model.predict(img)
+        bined = cv2.imread(cached_labels[image_id]['bin_file_name'])[:, :, 0]
+        bined = np.expand_dims(bined, axis=[0, 3])
+        labels = cv2.imread(cached_labels[image_id]['labels_file_name'])[:, :, 0]
+        labels = np.expand_dims(labels, axis=[0, 3])
+        model.fit(bined, labels)
