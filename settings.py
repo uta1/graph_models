bde17c8d167ccdfe0aae0ac585368e85538dbd89
uta1 <uta123@yandex-team.ru>
2021-04-14 14:09:05 +0300

    Refs

--- settings.py
+++ settings.py
@@ -4,25 +4,12 @@ from lib_imports import *
 @dataclasses.dataclass
 class Config:
     # Main
-    MODULES = ['prepare_samples', 'prepare_trg']
+    MODULES = ['prepare_samples', 'prepare_trg', 'train']
     MODE = 'samples'
-
-    LABELS = MODE + '.json'
 
     # Directories of data
     SRC_FOLDER = '../src/'
     TRG_FOLDER = '../trg/'
-
-    FOLDER = SRC_FOLDER + MODE + '/'
-
-    # Directories of labels and target images
-    BINS_FOLDER = TRG_FOLDER + MODE + '_bins/'
-    LABELS_FOLDER = TRG_FOLDER + MODE + '_labels/'
-    JSONS_FOLDER = TRG_FOLDER + MODE + '_jsons/'
-
-    # Labels creation
-    LABELS_PATH = SRC_FOLDER + LABELS
-    CACHED_LABELS_PATH = JSONS_FOLDER + 'cached_' + LABELS
 
     # Rects prediction
     MIN_OBJECT_WIDTH = 4
@@ -37,6 +24,43 @@ class Config:
 
     # Learning
     BATCH_SIZE = 1
+
+    # Name of src-file containing metainfo
+    # Available only for samples, train and val
+
+    @property
+    def LABELS(self):
+        return self.MODE + '.json'
+
+    @property
+    def LABELS_PATH(self):
+        return self.SRC_FOLDER + self.LABELS
+
+    # Directory of dataset tagged by mode
+
+    @property
+    def FOLDER(self):
+        return self.SRC_FOLDER + self.MODE + '/'
+
+    # Directories of labels and target images
+
+    @property
+    def BINS_FOLDER(self):
+        return self.TRG_FOLDER + self.MODE + '_bins/'
+
+    @property
+    def LABELS_FOLDER(self):
+        return self.TRG_FOLDER + self.MODE + '_labels/'
+
+    @property
+    def JSONS_FOLDER(self):
+        return self.TRG_FOLDER + self.MODE + '_jsons/'
+
+    # Path of trg-file containing metainfo redesigned for our needs
+
+    @property
+    def CACHED_LABELS_PATH(self):
+        return self.JSONS_FOLDER + 'cached_' + self.LABELS
 
 
 config = Config()
